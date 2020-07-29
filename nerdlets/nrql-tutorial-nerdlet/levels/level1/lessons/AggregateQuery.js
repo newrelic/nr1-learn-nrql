b225aa42371ae9a092f6c1f20046399d750ee5a2
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-07-29 11:33:58 -0700

    Suggest wording tweaks to maximize clarity/readability.

--- nerdlets/nrql-tutorial-nerdlet/levels/level1/lessons/AggregateQuery.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level1/lessons/AggregateQuery.js
@@ -5,16 +5,17 @@ export default function AggregateQuery() {
   return (
     <div>
       <p>
-        Rather than look at individual events you usually want to look at
-        summarized aggregate data across a large number of events. This is where
-        NRDB shines. NRDB can scan billions of events and give you instant
-        answers to your questions about the data in realtime.
+        Instead of viewing individual events, you usually want to view
+        summarized aggregate data across many events. This is where NRDB shines.
+        NRDB can scan billions of events and provide instant, realtime answers
+        to questions about your data.
       </p>
       <p>
-        For example, a single <code>Transaction</code> event has a{' '}
-        <code>duration</code> attribute which represents how long that call took
-        to execute in seconds. Let's see if we can discover the{' '}
-        <code>average</code> duration across all Transaction events:
+        For example, each <code>Transaction</code> event has a{' '}
+        <code>duration</code> attribute. This attribute represents how many
+        seconds the call took to execute. Let's see if we can discover the{' '}
+        <code>average</code> duration across all <code>Transaction</code>{' '}
+        events:
       </p>
       <SampleQuery
         nrql="SELECT **average(duration)** FROM Transaction "
@@ -22,39 +23,44 @@ export default function AggregateQuery() {
       />
 
       <p>
-        NRQL has many functions you can use to aggregate event data. Some of the
-        common functions you might use are <code>max()</code>,{' '}
-        <code>min()</code> and <code>percentile()</code>. In the following
-        example, we are going to ask NRDB to tell us what the duration of the
-        slowest transaction in the last 60 minutes was.
+        NRQL has many built-in functions you can use to aggregate event data.
+        Common functions you might use include <code>max()</code>,{' '}
+        <code>min()</code> and <code>percentile()</code>.
+      </p>
+      <p>
+        {' '}
+        In the following example, we ask NRDB for the duration of the slowest
+        transaction from the last 60 minutes.
       </p>
 
       <p>
-        So let's ask NRDB for the <code>max(duration)</code>.
+        To do this, we will ask NRDB for the <code>max(duration)</code>.
       </p>
 
       <p>
-        <em>Note: the default time frame for a query is the last 60 minutes</em>
+        <em>
+          Note: The default time frame for a query is the last 60 minutes.
+        </em>
       </p>
       <SampleQuery nrql="SELECT **max(duration)** FROM Transaction " span="6" />
 
       <p>
-        Now we know how long the slowest experience one of our customers had in
-        the last 60 minutes. Let's invert the logic: What about the fastest
-        experience? If we ask NRDB for <code>min(duration)</code>, we should be
-        able to answer that question.
+        We now know how long the slowest customer experience in the last 60
+        minutes took. Next, let's invert the logic: How long was the fastest
+        experience? If we ask NRDB for <code>min(duration)</code>, we can find
+        out.
       </p>
       <SampleQuery nrql="SELECT **min(duration)** FROM Transaction " span="6" />
 
       <p>
-        <code>max()</code> and <code>min()</code> are interesting. However, we
-        may want to perform some arithmetic operations on our data. With{' '}
-        <code>sum()</code> we can do a basic math addition on the values of a
-        numeric attribute. In this query, we are going to try a new attribute,{' '}
+        As you can see, <code>max()</code> and <code>min()</code> both return
+        interesting data points. However, we may also want to perform arithmetic
+        on our data. With <code>sum()</code> we can do basic addition on a
+        numeric attribute. In this query, we will use a new attribute,{' '}
         <code>databaseCallCount</code>. This attribute counts the number of
-        database calls each transaction makes. By summing we can get the total
-        volume of database calls for the default 60-minute time window. (This
-        will be 0 if your apps don't talk to a database)
+        database calls each transaction makes. By summing them we can get the
+        total volume of database calls in the default 60 minute time window.
+        (This will be 0 if your apps don't talk to a database.)
       </p>
       <SampleQuery
         nrql="SELECT **sum(databaseCallCount)** FROM Transaction "
@@ -62,17 +68,16 @@ export default function AggregateQuery() {
       />
 
       <p>
-        Finally, you can simply count all the Transaction events that have been
-        recorded. We do this with <code>count(*)</code>. In this example, we
-        count all the events recorded for all the applications reporting to New
-        Relic APM:
+        Finally, you can also simply count all recorded <code>Transaction</code>{' '}
+        events. We do this with <code>count(*)</code>. In this example, we count
+        all recorded events for all applications reporting to New Relic APM:
       </p>
       <SampleQuery nrql="SELECT **count(*)** FROM Transaction " span="6" />
 
       <h2>Lesson Summary</h2>
       <p>
         Now we're doing some really interesting things. We're measuring the
-        boundaries of our performance. We are grouping event data together and
+        boundaries of our performance, grouping event data together, and even
         calculating information about our chosen attribute.
       </p>
 
@@ -84,9 +89,8 @@ export default function AggregateQuery() {
       </p>
 
       <p>
-        Manipulating the data in this way is going to help us when we want to
-        present the data we need in different visualizations that will help us
-        spot performance issues.
+        Manipulating data like this will help us when we want to present data in
+        visualizations, which can assist us in spotting performance issues.
       </p>
     </div>
   );

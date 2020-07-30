52286a93ff4010b78ffe315173b6d9d5c77d31c2
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-07-30 13:32:00 -0700

    More minor wording suggestions, this time in the NextSteps level 1 lesson.

--- nerdlets/nrql-tutorial-nerdlet/levels/level1/lessons/Where.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level1/lessons/Where.js
@@ -5,15 +5,15 @@ export default function Where() {
   return (
     <div>
       <p>
-        Most of the time you want your query to operate on a subset of the data
-        in NRDB. For example, let's query for the average response time for our
-        backend application. In this data, there is an attribute called{' '}
-        <code>transactionType</code> that specifies whether the transaction we
-        timed was a "Non-Web" (maybe a cron-job or queued task) or "Web"
+        You'll often want your query to operate on a subset of NRDB data. For
+        example, let's query for our backend application's average response
+        time. This data includes an attribute called{' '}
+        <code>transactionType</code>, which specifies whether the transaction we
+        timed was a "Web", or "Non-Web" (such as a chron-job or queued task)
         transaction.
       </p>
       <p>
-        Let's filter our query to return data only for the "Web"
+        Let's filter our query to only return data for the "Web"
         transactionType:
       </p>
       <SampleQuery
@@ -27,9 +27,9 @@ export default function Where() {
       </p>
       <p>
         This query shows the average response time of "Web" transactions that
-        were faster than 100ms and had a response code of either 200 or 302.
-        Notice how we use the parentheses to control the <code>OR</code> which
-        may behave differently than you might expect.
+        were both faster than 100ms, and had a response code of either 200 or
+        302. Notice how we use parentheses to control the <code>OR</code>, which
+        may behave differently than you'd expect.
       </p>
       <SampleQuery
         nrql="SELECT average(duration) FROM Transaction WHERE transactionType='Web' **AND** duration < 0.1 **AND (**httpResponseCode=200 **OR** httpResponseCode=302**)** TIMESERIES"
@@ -38,17 +38,13 @@ export default function Where() {
 
       <h2>Lesson Summary</h2>
       <p>
-        This wasn't a long lesson but hopefully, it was nice and clear. So far,
-        every query we made was performing the actions on all the available data
-        in the given time window.
-      </p>
-
-      <p>
-        Filtering out undesired data is important. The examples above are useful
-        - we might want to filter to a type of transaction, or where a response
-        code is a specific value. Equally, you might filter to a specific{' '}
-        <code>appName</code> or Transaction <code>name</code> or even a custom
-        attribute which you've tagged on your data set.
+        So far, every query we've made has performed actions on all available
+        data in the given time window. But filtering out undesired data is also
+        important. The examples above are useful when we want to filter to a
+        type of transaction, or when a response code is a specific value.
+        Similarly, you could filter to a specific <code>appName</code>,
+        Transaction <code>name</code> or even a custom attribute you've tagged
+        on your data.
       </p>
     </div>
   );

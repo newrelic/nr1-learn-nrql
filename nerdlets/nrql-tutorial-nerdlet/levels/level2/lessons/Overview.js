00db099afce4b35323106107c95f084d8315b31b
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-07-30 14:56:24 -0700

    Suggest minor updates to Level 2 Overview wording and sentence structure.

--- nerdlets/nrql-tutorial-nerdlet/levels/level2/lessons/Overview.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level2/lessons/Overview.js
@@ -4,23 +4,21 @@ export default function Overview() {
   return (
     <div>
       <p>
-        Welcome to Level 2. In the previous level, we have explored the
-        fundamentals of building a basic query and taking some actions to
-        manipulate the results to get back the data we wanted. We covered the
-        basic structure required to query data, how to define the time window
-        and how to select the attributes we wanted to observe. We also began
-        looking at how to aggregate data into valuable information which can be
-        displayed on a line graph and other visualizations.
+        Welcome to Level 2. In the previous level, we explored the fundamentals
+        of building queries and manipulating results to retrieve the data we
+        want. We covered basic query structure, defining time windows, and how
+        to select specific attributes to observe. We also began learning how to
+        aggregate and display data on line graphs and other visualizations.
       </p>
 
       <p>
-        In this section, we are going to delve deeper into even more functions
-        for exploring interesting aggregations on our data. We are going to
-        investigate how to compare the returned data with a previous time
-        period, group data into more specific time windows, be more specific
-        with our SINCE and UNTIL functionality, and also learn how we can use
-        wildcards in our filters. We will also learn how we can rename
-        attributes to be more user-friendly.
+        In this section, we will delve deeper and explore even more functions
+        for creating interesting aggregations. We will learn how to compare
+        returned data with previous time periods, group data into more specific
+        time windows, be more granular with <code>SINCE</code> and{' '}
+        <code>UNTIL</code> functionality, and explore using wildcards in
+        filters. We will even discuss how to rename attributes to be more
+        user-friendly.
       </p>
 
       <p>Let's get started!</p>

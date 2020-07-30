a887490a02baab94b99fae1a3636dda2c6939563
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-07-30 14:41:59 -0700

    Suggest more tweaks in CompareWith.

--- nerdlets/nrql-tutorial-nerdlet/levels/level2/lessons/CompareWith.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level2/lessons/CompareWith.js
@@ -5,15 +5,21 @@ export default function CompareWith() {
   return (
     <div>
       <p>
-        We have learned how to use time ranges through the <code>SINCE</code>{' '}
-        and <code>UNTIL</code> clauses. What if we want to compare values from
-        different time ranges? We can achieve this with the{' '}
-        <code>COMPARE WITH</code> clause. We use <code>SINCE</code> and{' '}
-        <code>UNTIL</code> to define our period of interest and then specify the
-        time period we wish to compare to by specifying a{' '}
-        <code>COMPARE WITH [time period] AGO</code> clause with a relative
-        offset value. In our sample query, we are comparing the last day with
-        the week before by using a relative offset of <code>1 week ago</code>.
+        We have learned how to use time ranges with <code>SINCE</code> and{' '}
+        <code>UNTIL</code> clauses. But what if we want to compare values from
+        <em>different</em> time ranges? We can achieve this with the{' '}
+        <code>COMPARE WITH</code> clause.
+      </p>
+      <p>
+        We use <code>SINCE</code> and <code>UNTIL</code> to define our period of
+        interest. Then, we denote the time period we'd like to compare against
+        with a <code>COMPARE WITH [time period] AGO</code> clause containing a
+        relative offset value.
+      </p>
+
+      <p>
+        Specifically, in the sample query below we compare the last day against
+        the previous week using a relative offset of <code>1 week ago</code>.
       </p>
       <SampleQuery
         nrql="SELECT average(duration) FROM Transaction SINCE 1 DAY AGO **COMPARE WITH 1 WEEK AGO**"
@@ -21,10 +27,9 @@ export default function CompareWith() {
       />
 
       <p>
-        If you need to map the comparison of values over time, use{' '}
-        <code>TIMESERIES</code> to create a line chart that shows the
-        comparison. You are now able to visualize how your time window of
-        concern compares with recent data and track it over time.
+        To map the comparison of values over time, add <code>TIMESERIES</code>.
+        This creates a line chart of the comparison, allowing you to visualize
+        how this period compares to recent data, and track it over time.
       </p>
       <SampleQuery
         nrql="SELECT average(duration) FROM Transaction SINCE 1 DAY AGO **COMPARE WITH 1 WEEK AGO** TIMESERIES"
@@ -32,9 +37,9 @@ export default function CompareWith() {
       />
 
       <p>
-        You can specify many different relative time periods in the same format
-        as used with <code>UNTIL</code>. For instance, you may specify{' '}
-        <code>4 WEEKS AGO</code> or <code>6 HOURS AGO</code>
+        You can also specify many different relative time periods in the same
+        format, similar to <code>UNTIL</code>. For instance, you may specify{' '}
+        <code>4 WEEKS AGO</code> or <code>6 HOURS AGO</code>.
       </p>
       <SampleQuery
         nrql="SELECT average(duration) FROM Transaction SINCE 1 HOUR AGO **COMPARE WITH 6 HOURS AGO** TIMESERIES"
@@ -43,11 +48,11 @@ export default function CompareWith() {
 
       <h2>Lesson Summary</h2>
       <p>
-        Comparisons can quickly help you answer questions about what's happening
-        in your world right now. Are we up or down compared with last week for
-        sales or application performance, error rates or MTTR? If we are
-        investigating an issue, comparing with a <em>normal</em> period can be
-        very useful.
+        Comparisons can quickly answer questions about what's happening in your
+        applications. Are different sales, performance, MTTR, or error values up
+        or down compared to last week? And, if you are investigating an issue,
+        comparing aperiod of problemantic performance to a period of{' '}
+        <em>normal</em> performance can be very useful.
       </p>
     </div>
   );

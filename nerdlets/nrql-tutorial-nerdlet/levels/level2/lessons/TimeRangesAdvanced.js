55062d0a0ef8b9c03b6183d2c09afaf0f3e74f34
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-08-04 15:52:34 -0700

    Fix formatting inconsistencies via ESLint.

--- nerdlets/nrql-tutorial-nerdlet/levels/level2/lessons/TimeRangesAdvanced.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level2/lessons/TimeRangesAdvanced.js
@@ -18,43 +18,45 @@ export default function TimeRangesAdvanced() {
     <div>
       <p>
         The <code>SINCE</code> and <code>UNTIL</code> clauses are not limited to
-        using relative time ranges. You can specify a specific date and/or time
-        as well. In our sample query, we specify the since date in{' '}
-        <code>YYYY-MM-DD</code> format. This could be useful if you need to
-        create SLA reports for a specified period of time.
+        relative time ranges. You can also provide a specific date and/or time.
+        In the following sample query, we include a <code>SINCE</code> date in{' '}
+        <code>YYYY-MM-DD</code> format. This is useful when creating SLA reports
+        for a specified period of time.
       </p>
       <SampleQuery nrql={timedNRQL} span="12" />
 
       <p>
-        You can include a specific time too with the format{' '}
-        <code>YYYY-MM-DD HH:MM</code>. In this query, you can see the data being
+        You can even include specific time with the format{' '}
+        <code>YYYY-MM-DD HH:MM</code>. In this query, you can see the data is
         set at 6pm.
       </p>
       <SampleQuery nrql={timedNRQLTime} span="12" />
 
       <p>
         Sometimes, as an engineer, you might receive an event time in epoch
-        (unixtime) and New Relic does not want to force you to translate that
-        back to a date. Instead, we just accept epoch timestamps as an
-        acceptable value in <code>SINCE</code> and <code>UNTIL</code>
+        (unixtime). Conveniently, epoch timestamps are an acceptable value in{' '}
+        <code>SINCE</code> and <code>UNTIL</code> clauses, so you don't have to
+        translate these values into another date format.
       </p>
       <SampleQuery nrql={epochNRQL} span="12" />
 
       <p>
-        When NRDB shows data over a period of time, it assumes that you want to
-        see the data from the perspective of your timezone. But with dispersed
-        teams at an international level, your "today" could be someone’s
-        "tomorrow" or "yesterday" depending on their location.
+        When NRDB shows data over a period of time, it assumes you want to see
+        the data from the perspective of your timezone. But with dispersed
+        international teams, your "today" could be someone else's "tomorrow" or
+        "yesterday" depending on their location.
       </p>
 
       <p>
-        Use the <code>WITH TIMEZONE</code> clause to provide a specific timezone
-        that the data should be displayed from. This affects the interpretation
-        of the values in <code>SINCE</code> and <code>UNTIL</code> clauses. Take
-        the example of these two charts: each query has a specified timezone
-        using <code>WITH TIMEZONE</code> that are 8 hours apart. Notice that the
-        pattern of data is the same offset 8 hours to align with their
-        respective timezone.
+        You can use the <code>WITH TIMEZONE</code> clause to define a timezone
+        to display data from. This affects the interpretation of values in{' '}
+        <code>SINCE</code> and <code>UNTIL</code> clauses.
+      </p>
+      <p>
+        Consider the two example charts below. Each query has a specified
+        timezone using <code>WITH TIMEZONE</code>. The two are 8 hours apart.
+        Notice the pattern of data is the same, but offset 8 hours to align with
+        each respective timezone:
       </p>
       <SampleQuery
         nrql="SELECT count(\*) from Transaction since yesterday until today **WITH TIMEZONE 'America/Los_Angeles'** TIMESERIES"
@@ -68,10 +70,8 @@ export default function TimeRangesAdvanced() {
       <h2>Lesson Summary</h2>
       <p>
         Before this lesson, all our time control mechanisms were based on
-        relative from now. That's super functional for dynamic dashboards and
-        makes the dashboards constantly viewing the interesting data in the time
-        windows you need to observe. With the lessons today, we have learned how
-        to adjust the view depending where someone is in the world. Maybe a
+        relative times from now. With the lessons today, we have learned how to
+        adjust the view depending where someone is in the world. Maybe a
         customer in East Coast America reports an issue and your engineering
         team is in West Coast America. They can build a dashboard and translate
         the view to map to the timezone as a customer would be citing. So if a
@@ -79,12 +79,11 @@ export default function TimeRangesAdvanced() {
         at 9am we don't have to mentally translate.
       </p>
       <p>
-        Epoch and standard date stamps make sense. You may have been hoping
-        either of these existed and of course, you are right. So when you need
-        to focus your data to specific dates of an incident for example, and you
-        want to investigate the data without a moving time window being relative
-        to the current time, this knowledge will help you obtain data in a
-        static time window.
+        Epoch and standard date stamps make sense. So when you need to focus
+        your data to specific dates of an incident for example, and you want to
+        investigate the data without a moving time window being relative to the
+        current time, this knowledge will help you obtain data in a static time
+        window.
       </p>
     </div>
   );

488a4b129aad9ba11f41d78968d51b866cea49ff
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-08-04 10:28:31 -0700

    Suggest wording tweaks in level 3 Filter to Event Types lesson.

--- nerdlets/nrql-tutorial-nerdlet/levels/level3/lessons/FilterToEventTypes.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level3/lessons/FilterToEventTypes.js
@@ -5,14 +5,16 @@ export default function FilterToEventTypes() {
   return (
     <div>
       <p>
-        Now it's time to learn something not many New Relic customers use or
-        maybe are even aware of. We truly are in the realm of NRQL ninja land
-        right now. So far, we've made a query that pulls data from a single
-        source. However, what if I want to plot 2 data points but they are
-        stored as two different event types? Querying NRDB data is not limited
-        to a single event type, allowing you to see data representative of your
-        entire system rather than just one aspect. To query from multiple event
-        types, simply mention each event type seperated by a comma.
+        We're truly becoming NRQL ninjas! Next we'll explore something few New
+        Relic customers are even aware of: filtering to event types.
+      </p>
+
+      <p>
+        So far, we've made queries that pull data from a single source. But what
+        if you want to plot 2 data points that are stored as two different event
+        types? Querying NRDB data is not limited to a single event type! To
+        query from multiple event types you can include each event type
+        seperated by a comma.
       </p>
       <SampleQuery
         nrql="SELECT count(\*) as 'Combined Events' **FROM Transaction, PageView** SINCE  1 day ago"
@@ -20,11 +22,11 @@ export default function FilterToEventTypes() {
       />
 
       <p>
-        To make this more useful, the <code>eventType()</code> function allows
-        us to know which event type a record is from. We can use that to control
-        our data output. In this example, we can see the total number of{' '}
+        To make this even more useful, the <code>eventType()</code> function
+        tells us which event type a record is from. We can use this to control
+        our data output. In this example, we see the total number of{' '}
         <em>Transaction</em> and <em>PageView</em> events combined, as well as
-        the totals for just <em>Transaction</em> and just <em>PageView</em>.
+        the totals for only <em>Transaction</em> and <em>PageView</em>.
       </p>
       <SampleQuery
         nrql="SELECT count(\*) as 'Combined Events', filter(count(\*), **WHERE eventType() = 'PageView'**) as 'Page Views', filter(count(\*), **WHERE eventType()='Transaction'**) as 'Transactions' FROM Transaction, PageView SINCE  1 day ago"
@@ -32,15 +34,15 @@ export default function FilterToEventTypes() {
       />
 
       <p>
-        Let's look at this in more detail: <code>count(*)</code> is the total of
-        both <em>Transaction</em> and <em>PageView</em>. However, we use that
-        aggregator function <code>filter()</code> that we learned about recently
-        to do something unique. We tell it{' '}
-        <code>WHERE eventType()='PageView'</code>. Now the filter function
-        observed the type of event as part of the total result set and filtered
-        to only have those specific events. We can even plot this as a
-        timeseries to compare 2 data points that may be directly comparable on a{' '}
-        <code>TIMESERIES</code> line graph
+        Let's look at this in more detail: <code>count(*)</code> is the total
+        number of both <em>Transaction</em> and <em>PageView</em> events.
+        However, we can use the aggregator function <code>filter()</code> we
+        recently learned about to do something unique. We tell it{' '}
+        <code>WHERE eventType()='PageView'</code>. This invokes the filter
+        function to observe the event type as part of the total result set,
+        then filter to display only those specific events. We can even add
+        <code>TIMESERIES</code> to visualize 2 directly comparable data points
+        on a line graph.
       </p>
 
       <SampleQuery
@@ -50,11 +52,10 @@ export default function FilterToEventTypes() {
 
       <h2>Lesson Summary</h2>
       <p>
-        Now we have managed to get results and plot a line graph using data from
-        two different event types. This is just an example of how even without
-        the database concept of joins, NRQL is still extremely powerful and can
-        keep your results coming blazing fast without the complexity of joining
-        two data sets.
+        We have managed to locate, return, and graph data from two different
+        event types. This is an example of how NRQL can allow you to navigate
+        any necessary data quickly and succinctly; no complex joining or join
+        statements required!
       </p>
     </div>
   );

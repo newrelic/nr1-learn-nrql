8d5305c9a30552712fe602906835f3dc5034216b
Courtney Phillips <courtney.mae.phillips@gmail.com>
2020-08-04 11:12:23 -0700

    Suggest minor wording tweaks to level 3 Overview lesson.

--- nerdlets/nrql-tutorial-nerdlet/levels/level3/lessons/Overview.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/level3/lessons/Overview.js
@@ -4,23 +4,22 @@ export default function Overview() {
   return (
     <div>
       <p>
-        Welcome to Level 3. Thanks for sticking with us! We hope that seeing the
-        example queries and explanations with your own data helps you understand
-        how you can transform your event data to incredibly powerful visuals. Be
-        sure you try out some of the queries on your own data on different event
-        types to really understand their power.
+        Welcome to Level 3. Thanks for sticking with us! We hope seeing example
+        queries and explanations in the context of your own data helps you
+        better understand how to transform data into powerful visuals. (Try
+        queries from this course on data with different event types to really
+        understand their power!)
       </p>
       <p>
-        We are now moving on to more advanced concepts. These may not always be
-        the kind of functions or features you make use of on every dashboard,
-        but they will certainly be features you turn to when tackling specific
+        We are now moving on to even more advanced concepts. You may not use
+        these functions and features on every single dashboard, but they will
+        certainly come in handy when tackling specific problems and
         requirements.
       </p>
       <p>
-        In this level, we will be covering the concept of faceting by case, more
-        advanced aggregation functions, understanding the value of the
-        EXTRAPOLATE keyword, filtering aggregation functions and how to override
-        values.
+        In this level, we will cover faceting by case, advanced aggregation
+        functions, the value of the EXTRAPOLATE keyword, filtering aggregation
+        functions, and how to override values.
       </p>
     </div>
   );

401c30f10a21136963d25cb03605a10ff4f2b936
James Buchanan <jbuchanan@newrelic.com>
2020-08-12 17:04:30 +0100

    resources page

--- nerdlets/nrql-tutorial-nerdlet/levels/resources/index.js
+++ nerdlets/nrql-tutorial-nerdlet/levels/resources/index.js
@@ -1,8 +1,14 @@
 import RefIndex from './refIndex';
+import Links from './links';
+
 
 export default [
   {
     title: 'Function and Operator Index',
     component: RefIndex
+  },
+  {
+    title: 'Links and Resources',
+    component: Links
   }
 ];
